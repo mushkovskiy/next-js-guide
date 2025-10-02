810859e5d9265cb55b974bcec9b6b7a42ccc61b0
mushkovskiy <eumu2022@gmail.com>
2025-10-02 21:57:37 +0300

    hw-5 fix pr comments

--- src/app/rackets/top-10/page.tsx
+++ src/app/rackets/top-10/page.tsx
@@ -1,18 +1,20 @@
-import { Top10RacketListContainer } from "@/containers/top-10-racket-list-container/top-10-racket-list-container"
-import { Suspense } from "react";
-import { Top10RacketListFallback } from "../../../../shared/components/top-10-racket-list-fallback";
-import { getTop10Rackets } from "@/services/get-top-10-rackets";
-import { Metadata } from "next";
+import { Top10RacketListContainer } from '@/containers/top-10-racket-list-container/top-10-racket-list-container';
+import { Suspense } from 'react';
+import { Top10RacketListFallback } from '../../../../shared/components/top-10-racket-list-fallback';
+import { getTop10Rackets } from '@/services/get-top-10-rackets';
+import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-    title: 'Top 10 Rackets Page',
-    description: 'Top 10 Rackets page',
-  };  
+  title: 'Top 10 Rackets Page',
+  description: 'Top 10 Rackets page',
+};
 const Top10RacketsPage = () => {
-    const top10RacketsPromise = getTop10Rackets();
-    return <Suspense fallback={<Top10RacketListFallback />}>
-        <Top10RacketListContainer top10RacketsPromise={top10RacketsPromise} />
+  const top10RacketsPromise = getTop10Rackets();
+  return (
+    <Suspense fallback={<Top10RacketListFallback />}>
+      <Top10RacketListContainer top10RacketsPromise={top10RacketsPromise} />
     </Suspense>
-}
+  );
+};
 
 export default Top10RacketsPage;
